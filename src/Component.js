3337e86a33c54a43b4cfc14b4b5129e3a14c4c2f
DianaRobayo <dmrcupn92@gmail.com>
2024-05-15 21:02:40 -0500

    Changes in component.js

--- src/Component.js
+++ src/Component.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Component() {
+  //propiedad
+  const [text, setText] = useState();
+  const [updated, setUpdated] = useState();
+
+  //enf o nfn permite crear la funcion flecha
+  const textOnChange = (event) => {
+    setText(event.target.value)
+  }
+
+  //Se le envia el texto
+  const buttonOnClick = () => { 
+    setUpdated(text)
+  }
+
   return (
-    <div>My Component</div>
+    <div>
+      <input type="text" value={text} onChange={textOnChange} />
+      <button onClick={buttonOnClick}>Actualizar</button>
+      <p>Texto input {text}</p>
+      <p>Texto actualizado {updated}</p>
+    </div>
   )
 }
